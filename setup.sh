a1572afc14f185cb512705eec13707654f2be067
KyleDeng <55138677+KyleDeng@users.noreply.github.com>
2024-11-20 18:19:37 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -132,13 +132,31 @@ sudo vi /etc/samba/smb.conf
 sudo smbpasswd -a huatuo
 sudo service smbd restart
 
+# 输入法
+# 1. 打开系统设置（ “Settings”）。
+# 2. 选择地区和语言（“Region & Language”）。
+# 3. 点击“Manage Installed Languages”。
+# 4. 点击“Install/Remove Languages”勾选Chinese，可以同时勾选简体中文和繁体中文。
+# 5. 点击“Apply Changes”。这需要管理员权限，你需要输入密码或请求的管理员账户的密码。
+# 6. 中文正在被安装，相应的中文输入法也会一并被安装。安装完成后，你需要先退出登录，重新登录后新的语言选项才会被加载。
+# 7. 重新登录后，再次打开系统设置（“Settings”）。
+# 8. 选择 “Keyboard”。
+# 9. 在 “Input Sources”下，点击 “+” 号来添加新的输入法。
+# 10. 点击“Chinese”，并选择 “Chinese (Intelligent Pinyin)”。
+# 11. 点击“Add”，智能拼音输入法就被成功加入到输入法之中了。
+
 # 输入法 搜狗
 https://pinyin.sogou.com/linux/help.php
 sudo apt-get install fcitx
-打开 系统设置——区域和语言——管理已安装的语言——在“语言”tab下——点击“添加或删除语言”
-弹出“已安装语言”窗口，勾选中文（简体），点击应用
-回到“语言支持”窗口，在键盘输入法系统中，选择“fcitx”
-sudo dpkg -i sogoupinyin_版本号_amd64.deb
+# 打开 系统设置——区域和语言——管理已安装的语言——在“语言”tab下——点击“添加或删除语言”
+# 弹出“已安装语言”窗口，勾选中文（简体），点击应用
+# 回到“语言支持”窗口，在键盘输入法系统中，选择“fcitx”
+sudo cp /usr/share/applications/fcitx.desktop /etc/xdg/autostart/  # fcitx开机自启动
+sudo apt purge ibus  # 删除ibus框架
+sudo dpkg -i sogoupinyin_版本号_amd64.deb  # 安装搜狗
+sudo apt install libqt5qml5 libqt5quick5 libqt5quickwidgets5 qml-module-qtquick2  # 安装依赖
+sudo apt install libgsettings-qt1
+# 重启
 
 # poetry
 pip3 install poetry
