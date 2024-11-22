d1d2a5e76cf2a17f557fb77b1491d8be31b92d4d
KyleDeng <dlkxiaok@163.com>
2024-11-22 11:29:08 +0800

    main.c

--- main.c
+++ main.c
@@ -1,11 +1,21 @@
 #include <stdio.h>
 
+int say_hello(const char* name)
+{
+    printf("Hello, %s\n", name);
+
+    return 0;
+}
+
 int main(int argc, char* argv[])
 {
     int i;
+    const char* name = "KK";
+
     for(i = 0; i < argc; i++) {
         printf("[%d]: %s\n", i, argv[i]);
     }
+    say_hello(name);
 
     return 0;
 }
